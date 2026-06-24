d1894de4bab339f7acbeba9617eb615f63ae4eab
projectgurpreet <projectgurpreet@gmail.com>
2026-06-24 11:12:22 +0530

    secret code language quiz is finally done

--- secretCodeLanguage.py
+++ secretCodeLanguage.py
@@ -33,74 +33,127 @@ import random
 class TaskError(Exception):
     pass
 
-alphabets = "abcdefghijklmnopqrstuvwxyz"
+# alphabets = "abcdefghijklmnopqrstuvwxyz"
 
-def randomAssign(assigningList):
-    for i in range(6):
-        if i<3:
-            assigningList.insert(0,random.choice(alphabets))
-        else:
-            assigningList.insert(len(assigningList),random.choice(alphabets))
-    return assigningList
+# def randomAssign(assigningList):
+#     for i in range(6):
+#         if i<3:
+#             assigningList.insert(0,random.choice(alphabets))
+#         else:
+#             assigningList.insert(len(assigningList),random.choice(alphabets))
+#     return assigningList
 
-def coding(message):
-    messageLength = len(message)
-    if messageLength<3:
-        tempList = list(message)
-        tempList.reverse()
-        codedMessage = "".join(tempList)
-        print(f"Your Message In The Secret Language is Given Below:\n{codedMessage}")
-    else:
-        tempList = list(message)
-        firstLetter = tempList[0]
-        tempList.pop(0)
-        tempList.append(firstLetter)
-        codedMessage = "".join(randomAssign(tempList))
-        print(f"Your Message Coded In The Secret Language is Given Below:\n{codedMessage}")
+# def coding(message):
+#     messageLength = len(message)
+#     if messageLength<3:
+#         tempList = list(message)
+#         tempList.reverse()
+#         codedMessage = "".join(tempList)
+#         print(f"Your Message In The Secret Language is Given Below:\n{codedMessage}")
+#     else:
+#         tempList = list(message)
+#         firstLetter = tempList[0]
+#         tempList.pop(0)
+#         tempList.append(firstLetter)
+#         codedMessage = "".join(randomAssign(tempList))
+#         print(f"Your Message Coded In The Secret Language is Given Below:\n{codedMessage}")
 
 
-def decoding(message):
-    messageLength = len(message)
-    if messageLength<3:
-        tempList = list(message)
-        tempList.reverse()
-        decodedMessage = "".join(tempList)
-        print(f"Your Message Decoded From The Secret Language is Given Below:\n{decodedMessage}")
-    else: 
-        tempList = list(message)
-        for i in range(6):
-            if i<3:
-                tempList.pop(0)
-            else:
-                tempList.pop(len(tempList)-1)
-        originalFirstLetter = tempList[len(tempList)-1]
-        tempList.pop()
-        tempList.insert(0,originalFirstLetter)
-        decodedMessage = "".join(tempList)
-        print(f"Your Message Decoded From The Secret Language is Given Below:\n{decodedMessage}")
+# def decoding(message):
+#     messageLength = len(message)
+#     if messageLength<3:
+#         tempList = list(message)
+#         tempList.reverse()
+#         decodedMessage = "".join(tempList)
+#         print(f"Your Message Decoded From The Secret Language is Given Below:\n{decodedMessage}")
+#     else: 
+#         tempList = list(message)
+#         for i in range(6):
+#             if i<3:
+#                 tempList.pop(0)
+#             else:
+#                 tempList.pop(len(tempList)-1)
+#         originalFirstLetter = tempList[len(tempList)-1]
+#         tempList.pop()
+#         tempList.insert(0,originalFirstLetter)
+#         decodedMessage = "".join(tempList)
+#         print(f"Your Message Decoded From The Secret Language is Given Below:\n{decodedMessage}")
 
-task = input("Do You Want To Code or Decode?: ")
-# if not task.title() == "Code" or not task.title() == "Decode":
-#     raise TaskError("Invalid Task Was Assigned!\nAllowed Tasks: 'Code' or 'Decode'")
-# elif task.title() == "Code":
-#     coding_message = input("What do you want to code in the secret language?: ")
-#     coding(coding_message)
-# elif task.title() == "Decode":
-#     decoding_message = input("What do you want to decode from the secret language?: ")
-#     decoding(decoding_message)
-# else:
-#     raise UnknownError("An Unknown Error Occured While Executing")
+# task = input("Do You Want To Code or Decode?: ")
+# # if not task.title() == "Code" or not task.title() == "Decode":
+# #     raise TaskError("Invalid Task Was Assigned!\nAllowed Tasks: 'Code' or 'Decode'")
+# # elif task.title() == "Code":
+# #     coding_message = input("What do you want to code in the secret language?: ")
+# #     coding(coding_message)
+# # elif task.title() == "Decode":
+# #     decoding_message = input("What do you want to decode from the secret language?: ")
+# #     decoding(decoding_message)
+# # else:
+# #     raise UnknownError("An Unknown Error Occured While Executing")
      
 
-if task.title() == "Code":
-    coding_message = input("What do you want to code in the secret language?: ").lower().strip()
-    coding(coding_message)
-elif task.title() == "Decode":
-    decoding_message = input("What do you want to decode from the secret language?: ").lower().strip()
-    decoding(decoding_message)
-else:
-    raise TaskError("Invalid Task Was Assigned!\nAllowed Tasks: 'Code' or 'Decode'")
+# if task.title() == "Code":
+#     coding_message = input("What do you want to code in the secret language?: ").lower().strip()
+#     coding(coding_message)
+# elif task.title() == "Decode":
+#     decoding_message = input("What do you want to decode from the secret language?: ").lower().strip()
+#     decoding(decoding_message)
+# else:
+#     raise TaskError("Invalid Task Was Assigned!\nAllowed Tasks: 'Code' or 'Decode'")
 
 
 
 #! But it was working fine. The main issue was that when I watched the solution video, there was a pretty short and fascinating solution, but it depends on what clicked in my mind at that point. One thing is for sure: I'll try to replicate in that way too using my own mind and see how things work. 
+
+
+#* tryin with course's approach
+import string
+lowercase_alphabets = string.ascii_lowercase
+
+def random_assigner(message):
+    random_start = ""
+    random_end = ""
+    for i in range(3):
+        random_start = random_start + random.choice(lowercase_alphabets)
+        random_end = random_end + random.choice(lowercase_alphabets)
+    message = random_start + message + random_end
+    return message
+
+task = input("Do You Want To Code or Decode?: ")
+coding = True
+
+if task.title() == "Code":
+    given_string = input("What do you want to code in the secret language?: ").strip()
+    coding = True
+elif task.title() == "Decode":
+    given_string = input("What do you want to decode from the secret language?: ").strip()
+    coding = False
+else:
+    raise TaskError("Invalid Task Was Assigned!\nAllowed Tasks: 'Code' or 'Decode'")
+
+# given_string = input("Enter your input: ").strip()
+words = given_string.split(" ")
+
+if(coding):
+    new_words = []
+    for word in words:
+        if(len(word)>=3):
+            word = word[1:] + word[0]
+            new_word = random_assigner(word)
+            new_words.append(new_word)
+        else:
+            new_words.append(word[::-1])    #* reverses a string
+    final_string = " ".join(new_words) 
+else:
+    new_words = []
+    for word in words:
+        if(len(word)>=3):
+            word = word[3:-3]
+            new_word = word[-1]+word[:-1]
+            new_words.append(new_word)
+        else:
+            new_word = word[::-1]
+            new_words.append(new_word)
+    final_string = " ".join(new_words) 
+
+print(final_string)
